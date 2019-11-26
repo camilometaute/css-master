52321a5642a061c6c2da1da7ada915979aaa0208
Camilo Metaute <camilometaute@lahaus.com>
2019-11-25 23:04:05 -0500

    grid congrats message

--- src/components/WebSiteCssGrid.vue
+++ src/components/WebSiteCssGrid.vue
@@ -25,7 +25,24 @@
       </div>
 
       <section class="homes">
-        Homes
+        <div class="card">
+          Card 1
+        </div>
+        <div class="card">
+          Card 2
+        </div>
+        <div class="card">
+          Card 3
+        </div>
+        <div class="card">
+          Card 4
+        </div>
+        <div class="card">
+          Card 5
+        </div>
+        <div class="card">
+          Card 6
+        </div>
       </section>
 
       <section class="gallery">
@@ -35,6 +52,33 @@
       <footer class="footer">
         Footer
       </footer>
+
+      <div class="container-message">
+        <div class="thanks-message">
+          <div class="letter">
+            <div class="black"></div>
+            <div class="white"></div>
+          </div>
+          <div class="letter">
+            R
+          </div>
+          <div class="letter">
+            A
+          </div>
+          <div class="letter">
+            C
+          </div>
+          <div class="letter">
+            I
+          </div>
+          <div class="letter">
+            A
+          </div>
+          <div class="letter">
+            S
+          </div>
+        </div>
+      </div>
     </section>
 </template>
 
