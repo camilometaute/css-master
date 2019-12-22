0b7d8adab693c290d084215a6dd426ab8c837bd3
Camilo Metaute <camilometaute@lahaus.com>
2019-12-21 21:53:42 -0500

    head section

--- src/components/OpendoorAdviser.vue
+++ src/components/OpendoorAdviser.vue
@@ -1,7 +1,44 @@
 <template>
-    <section class="opendoor-adviser">
-      HelloWorld
-    </section>
+    <div class="opndr">
+      <div class="opndr__head">
+        <div class="opndr__head--info">
+          <div class="opndr__head--info--naming">
+            <h2>Trish Elliott</h2>
+            <h4>Buying agent</h4>
+          </div>
+
+          <div class="opndr__head--info--status">
+            <div class="opndr__head--info--status--rating">
+              <h5>4.9</h5>
+              <span>
+                <font-awesome-icon icon="star" class="icon-rating-star" /></i>
+                <font-awesome-icon icon="star" class="icon-rating-star" /></i>
+                <font-awesome-icon icon="star" class="icon-rating-star" /></i>
+                <font-awesome-icon icon="star" class="icon-rating-star" /></i>
+                <font-awesome-icon icon="star" class="icon-rating-star" /></i>
+              </span>
+            </div>
+
+            <div class="opndr__head--info--status--review">
+              <h5>3</h5>
+              <span>reviews</span>
+
+            </div>
+
+            <div class="opndr__head--info--status--offers">
+              <h5>14</h5>
+              <span>accepted offers</span>
+
+            </div>
+          </div>
+
+        </div>
+        <div class="opndr__head--pic">
+          <img class="opndr__head--pic--agent" src="https://s3st0.buy.opendoor.com/images/160/160/Trish_Elliot_2019-03-21_1553211590.png" alt="">
+          <img class="opndr__head--pic--bg" src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='336' height='302' viewBox='0 0 336 302'%3E %3Cpath fill='%231C85E8' fill-rule='evenodd' d='M1021.14305,153.906702 L1097.02776,475.525217 C1099.31065,485.20067 1093.31779,494.894821 1083.64234,497.177709 C1081.86934,497.596042 1080.04311,497.742806 1078.22606,497.612986 L905.55954,485.276774 C895.64369,484.568333 888.17961,475.955643 888.888051,466.039793 C888.987338,464.650093 889.247638,463.276618 889.66372,461.946956 L986.445524,152.664654 C989.414376,143.177194 999.512212,137.892818 1008.99967,140.861671 C1015.06719,142.760341 1019.68307,147.718958 1021.14305,153.906702 Z' transform='rotate(-130 540.186 450.691)'/%3E %3C/svg%3E" alt="">
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
