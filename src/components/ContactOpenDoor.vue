6386c3a09e9b4e43c6a75e34153fb1f47685cd3c
Camilo Metaute <camilometaute@lahaus.com>
2020-01-11 11:51:10 -0500

    Building the tips section

---
+++ src/components/ContactOpenDoor.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="main-contact-opendoor">
+    <div class="side-tips">
+      <img src="https://imgdrop.imgix.net/168a4a828d6248e8b47313ff7a42e89d.svg" alt="">
+      <h4>Get expert tips in your inbox</h4>
+      <p>Discover ways to increase your home's value and stay on top of local trends.</p>
+      <input placeholder="you@example.com">
+      <button>Give me tips</button>
+    </div>
+    <div class="side-contact">
+      <div class="contact-us">
+        <h4>Contact Us</h4>
+        <p>contact@opendoor.com</p>
+        <p>1.888.352.7075</p>
+        <span>Customer service — (6am-7pm PT)</span>
+      </div>
+
+      <div class="locations">
+        <p>🇺🇸 Locations</p>
+        <ul>
+          <li><a href="#">Atlanta</a></li>
+          <li><a href="#">Austin, TX</a></li>
+          <li><a href="#">Charlote</a></li>
+          <li><a href="#">Dallas</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AirbnbCards'
+};
+</script>
