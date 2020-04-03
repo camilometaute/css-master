f82239baf6b93e512986902ef5cb46110320bcd7
Camilo Metaute <camilometaute@lahaus.com>
2020-04-02 21:41:45 -0500

    OpenDoor section with responsive

---
+++ src/components/OpenDoorPictureSection.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="open-door-picture-section">
+    <div class="open-door-picture-section__container">
+      <div class="open-door-picture-section__box">
+        <svg width="22px" height="14px" viewBox="0 0 27 19" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+					<g id="Page-2" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+						<g transform="translate(-213.000000, -2024.000000)" fill="#1C85E8" fill-rule="nonzero">
+							<path d="M219.610694,2043 C216.267355,2043 213,2040.6627 213,2036.06349 C213,2029.95635 217.103189,2025.65873 224.929644,2024 L225.461538,2025.65873 C220.598499,2027.84524 218.546904,2030.71032 218.622889,2033.72619 C219.002814,2033.4246 219.838649,2033.04762 220.826454,2033.04762 C223.409944,2033.04762 225.385553,2034.93254 225.385553,2038.02381 C225.385553,2041.11508 222.726079,2043 219.610694,2043 Z M234.149156,2043 C230.805816,2043 227.538462,2040.6627 227.538462,2036.06349 C227.538462,2029.95635 231.641651,2025.65873 239.468105,2024 L240,2025.65873 C235.136961,2027.84524 233.085366,2030.71032 233.161351,2033.72619 C233.541276,2033.4246 234.377111,2033.04762 235.364916,2033.04762 C237.948405,2033.04762 239.924015,2034.93254 239.924015,2038.02381 C239.924015,2041.11508 237.26454,2043 234.149156,2043 Z"></path>
+						</g>
+					</g>
+				</svg>
+        <h4>I loved that I could trade in my house to Opendoor. I made an offer on the second Opendoor house I saw and sold my old one at the same time..</h4>
+        <div class="open-door-picture-section__box__quote">
+          <span>Chantelle Miller | Sold and bought with Opendoor</span>
+        </div>
+      </div>
+    </div>
+    <div class="open-door-picture-section__bg-img"></div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'OpenDoorPictureSection'
+};
+</script>
