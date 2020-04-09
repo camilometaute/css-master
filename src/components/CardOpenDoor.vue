dfc93db419a867baf70816e442d690174a6dbbb8
Camilo Metaute <camilometaute@lahaus.com>
2020-04-09 15:56:21 -0500

    open door srp ready in markup with cards and something more

--- src/components/CardOpenDoor.vue
+++ src/components/CardOpenDoor.vue
@@ -1,6 +1,9 @@
 <template>
     <section class="card-open-door">
-      <div class="card-open-door__img">
+      <div
+        class="card-open-door__img"
+        :style="{ backgroundImage: 'linear-gradient(0deg, rgba(20,20,20,0.9) 10%, rgba(255,255,255,0.1) 95%), url(' +listItems.imgUrl+ ')' }"
+        >
         <span>
           <strong>{{listItems.price}}</strong> {{listItems.priceM2}}
         </span>
@@ -25,7 +28,8 @@ export default {
           price: '$219,000',
           priceM2: '$154',
           name: '1106 Henderson Dr. Leander',
-          feat: '3 bed 2 bath · 1,408 ft²'
+          feat: '3 bed 2 bath · 1,408 ft²',
+          imgUrl: 'https://opendoor.imgix.net/https%3A%2F%2Flisting-photos-production.s3.amazonaws.com%2Fuploads%2Fphotography_request-44656%2F20093569-Xlv4lQzVFwc.jpg?ixlib=rb-1.1.0&w=760&h=480&auto=compress&fit=crop&s=c2dd2037766fd5dffbba95f96c62a03d'
         }
       }
     }
--- src/components/SrpOpenDoor.vue
+++ src/components/SrpOpenDoor.vue
@@ -1,16 +1,25 @@
 <template>
     <section class="srp-open-door">
-      <div class="srp-open-door__left">
-        <div class="srp-open-door__box">
+      <div class="srp-open-door__head">
 
-        </div>
       </div>
-      <div class="srp-open-door__right">
-        <card-open-door
-          v-for="(items, i) in cardsData"
-          :listItems="cardsData[i]"
-          >
-        </card-open-door>
+      <div class="srp-open-door__body">
+        <div class="srp-open-door__left">
+          <div class="srp-open-door__box">
+            <span>Map here</span>
+          </div>
+        </div>
+        <div class="srp-open-door__right">
+          <div class="srp-open-door__right__head">
+            <h4>House for sale in austin</h4>
+            <span>For sale</span>
+          </div>
+          <card-open-door
+            v-for="(items, i) in cardsData"
+            :listItems="cardsData[i]"
+            >
+          </card-open-door>
+        </div>
       </div>
     </section>
 </template>
@@ -26,49 +35,57 @@ export default {
           price: '$219,000',
           priceM2: '$154',
           name: '1106 Henderson Dr. Leander',
-          feat: '3 bed 2 bath · 1,408 ft²'
+          feat: '3 bed 2 bath · 1,408 ft²',
+          imgUrl: 'https://bit.ly/2VcxvSm'
         },
         {
           price: '$249,000',
           priceM2: '$154',
           name: '1106 Henderson Dr. Leander',
-          feat: '3 bed 2 bath · 1,408 ft²'
+          feat: '3 bed 2 bath · 1,408 ft²',
+          imgUrl: 'https://bit.ly/34nszOR'
         },
         {
           price: '$289,000',
           priceM2: '$154',
           name: '1106 Henderson Dr. Leander',
-          feat: '3 bed 2 bath · 1,408 ft²'
+          feat: '3 bed 2 bath · 1,408 ft²',
+          imgUrl: 'https://bit.ly/3eaxLtO'
         },
         {
           price: '$299,000',
           priceM2: '$154',
           name: '1106 Henderson Dr. Leander',
-          feat: '3 bed 2 bath · 1,408 ft²'
+          feat: '3 bed 2 bath · 1,408 ft²',
+          imgUrl: 'https://bit.ly/2RGydqf'
         },
         {
           price: '$219,000',
           priceM2: '$154',
           name: '1106 Henderson Dr. Leander',
-          feat: '3 bed 2 bath · 1,408 ft²'
+          feat: '3 bed 2 bath · 1,408 ft²',
+          imgUrl: 'https://bit.ly/34psfij'
         },
         {
           price: '$249,000',
           priceM2: '$154',
           name: '1106 Henderson Dr. Leander',
-          feat: '3 bed 2 bath · 1,408 ft²'
+          feat: '3 bed 2 bath · 1,408 ft²',
+          imgUrl: 'https://bit.ly/3eaxLtO'
         },
         {
           price: '$289,000',
           priceM2: '$154',
           name: '1106 Henderson Dr. Leander',
-          feat: '3 bed 2 bath · 1,408 ft²'
+          feat: '3 bed 2 bath · 1,408 ft²',
+          imgUrl: 'https://bit.ly/2RGydqf'
         },
         {
           price: '$299,000',
           priceM2: '$154',
           name: '1106 Henderson Dr. Leander',
-          feat: '3 bed 2 bath · 1,408 ft²'
+          feat: '3 bed 2 bath · 1,408 ft²',
+          imgUrl: 'https://bit.ly/2VcxvSm'
         }
       ]
     }
