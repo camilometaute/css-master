ecd9f3a4b249da5005cc85cada90e6ef6e78ed3b
Camilo Metaute <camilometaute@lahaus.com>
2020-04-25 12:04:09 -0500

    some practice with vue to reforce logic with watchers and computeds

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="training-js">
-    <div class="training-container">
+    <section class="training-container">
       <div class="training-container__head">
         <h1>Agregando data con clicks de usuario</h1>
         <button @click="showCurrentDate">Mostrar fecha actual</button>
@@ -8,9 +8,9 @@
       <div class="training-container__body">
 
       </div>
-    </div>
+    </section>
 
-    <div class="training-container">
+    <section class="training-container">
       <div class="training-container__head">
         <h1>Práctica de computed</h1>
       </div>
@@ -20,16 +20,29 @@
           <li  v-for="name in filterByNameAction">{{name}}</li>
         </ul>
       </div>
-    </div>
+    </section>
 
-    <div class="training-container">
+    <section class="training-container">
       <div class="training-container__head">
         <h1>Práctica de watchers</h1>
       </div>
       <div class="training-container__body">
-        <input type="number" name="" value="" v-model.number="counter">
+        <div class="increase_element">
+          <button @click="decreaseValue" type="button" name="button">-</button>
+          <input type="number" name="" value="" min="0" max="8" v-model.number="counter">
+          <button @click="increaseValue" type="button" name="button">+</button>
+        </div>
+        <h4 class="current-bike">{{currentBike}}</h4>
       </div>
-    </div>
+    </section>
+
+    <section class="training-container">
+      <div class="training-container__body">
+        <input v-model="currentDate" type="date" name="" value="">
+        <button type="button" name="button" @click="saveDatePicker">Save Date</button>
+        <h4 v-show="showDate">Date saved: {{currentDate}}</h4>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -46,7 +59,15 @@ export default {
         'Jen Looper',
         'Miriam Suzanne'
       ],
-      counter: 0
+      counter: 0,
+      bikes: [
+        'MT 09',
+        'ECO 100',
+        'YZ 125'
+      ],
+      currentBike: '',
+      currentDate: '',
+      showDate: false
     }
   },
   methods: {
@@ -57,6 +78,17 @@ export default {
       mySpan.textContent = Date();
       mySpan.classList.add("item-date");
       trainingContainerBody.appendChild(mySpan);
+    },
+    decreaseValue() {
+      this.counter -= 1;
+      this.counter <= 0 ? this.counter = 8 : null;
+    },
+    increaseValue() {
+      this.counter += 1;
+      this.counter >= 9 ? this.counter = 0 : null;
+    },
+    saveDatePicker() {
+      this.showDate = !this.showDate;
     }
   },
   computed: {
@@ -68,6 +100,16 @@ export default {
   watch: {
     counter() {
       console.log('The counter was changed! by', this.counter);
+      if (this.counter > 3) {
+        this.currentBike = this.bikes[1];
+        console.log('Ahora current bike es:',this.currentBike);
+      } else if (this.counter < 3 ) {
+        this.currentBike = this.bikes[0];
+        console.log('Ahora current bike es:',this.currentBike);
+      } else {
+        this.currentBike = this.bikes[2];
+        console.log('Ahora current bike es:',this.currentBike);
+      }
     }
   }
 };
@@ -76,6 +118,51 @@ export default {
 <style lang="scss">
 .training-js {
   height: 80vh;
+
+  .current-bike {
+    background-color: #cbf3f0;
+    color: #2ec4b6;
+    font-size: 1.6rem;
+    margin: 0 auto;
+    padding: 3rem;
+    text-align: center;
+    width: 400px;
+  }
+
+  .increase_element {
+    align-items: center;
+    display: flex;
+    margin: 0 auto;
+    width: 600px;
+    margin-bottom: 2rem;
+
+    input {
+      border-radius: .6rem;
+      border: 1px solid #d3d3d3;
+      flex: 1;
+      font-size: 1.6rem;
+      height: 40px;
+      margin: 0 .2rem;
+      padding: .8rem;
+      text-align: center;
+    }
+
+    button {
+      background: #2ec4b6;
+      border-radius: .6rem;
+      border: none;
+      color: white;
+      cursor: pointer;
+      flex: 0;
+      font-size: 1.4rem;
+      height: 40px;
+      padding: 0 1.4rem;
+
+      &:hover {
+        background-color: #cbf3f0;
+      }
+    }
+  }
 
   .training-container {
     display: flex;
@@ -102,15 +189,6 @@ export default {
       display: flex;
       flex-direction: column;
       flex: 0 0 100%;
-
-      input {
-        border-radius: .6rem;
-        border: 1px solid #d3d3d3;
-        height: 40px;
-        margin: 2rem auto;
-        padding: .8rem;
-        width: 200px;
-      }
 
       ul {
         background-color: #d3d3d3;
