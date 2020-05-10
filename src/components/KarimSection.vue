4af31f1c8c3ed9257ffb9a74208859e4f962be61
Camilo Metaute <camilometaute@lahaus.com>
2020-05-10 13:07:11 -0500

    karim menu

--- src/components/KarimSection.vue
+++ src/components/KarimSection.vue
@@ -7,11 +7,42 @@
     <div class="karim-section__right">
       <img src="http://www.karimrashid.com/media/covers/fup2piel6.jpg" alt="">
     </div>
+    <button class="karim-section__btn" @click="openNavMenu">Menu</button>
+    <nav class="karim-section__nav" :class="{'active-nav' : menuIsActive}">
+      <button @click="closeNavMenu" class="karim-section__nav__close">X</button>
+      <ul>
+        <li>Work</li>
+        <li>Clients</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <input type="text" placeholder="Search" name="" value="">
+      <div class="karim-section__nav__bottom-links">
+        <a href="#">Facebook</a>
+        <a href="#">Twitter</a>
+        <a href="#">Instagram</a>
+        <a href="#">Youtube</a>
+      </div>
+    </nav>
   </section>
 </template>
 
 <script>
 export default {
-  name: 'KarimSection'
+  name: 'KarimSection',
+  data:function() {
+    return {
+      menuIsActive: false
+    }
+  },
+  methods: {
+    closeNavMenu() {
+      this.menuIsActive = false;
+
+    },
+    openNavMenu() {
+      this.menuIsActive = true;
+    }
+  }
 };
 </script>
