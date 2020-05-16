ad5375b11fa2c908103eae75a2b9a5d2545397d2
Camilo Metaute <camilometaute@lahaus.com>
2020-05-16 18:23:52 -0500

    changes background and other styles

--- src/components/StripeCarousel.vue
+++ src/components/StripeCarousel.vue
@@ -5,11 +5,11 @@
       </div>
       <div class="stripe-carousel__right">
         <p>We integrated Terminal in only a few sprints with three developers, not a team of 20 or 30</p>
-        <a href="#">Read the universe case study > </a>
         <h5><strong>JOSHUA KELLY</strong></h5>
         <h5>
           CEO
         </h5>
+        <a href="#">Read the universe case study > </a>
 
       </div>
     </section>
