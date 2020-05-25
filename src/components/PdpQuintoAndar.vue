62c2c60d7b983d90a86c0b40b911e733a5eac2c7
Camilo Metaute <camilometaute@lahaus.com>
2020-05-24 23:07:36 -0500

    sticky element first markup

--- src/components/PdpQuintoAndar.vue
+++ src/components/PdpQuintoAndar.vue
@@ -1,13 +1,29 @@
 <template>
-    <section class="content-pdp-qnta">
-      <div class="content-pdp-qnta__slider">
+    <div class="content-pdp-qnta">
+      <section class="content-pdp-qnta__slider">
         <button class="content-pdp-qnta__slider__btn content-pdp-qnta__slider__btn--left"><font-awesome-icon icon="chevron-left"  /></button>
         <button class="content-pdp-qnta__slider__btn content-pdp-qnta__slider__btn--right"><font-awesome-icon icon="chevron-right"  /></button>
         <div class="content-pdp-qnta__slider__item" v-for="item in imgSlider">
           <img :src="item.src" alt="">
         </div>
-      </div>
-    </section>
+      </section>
+      <section class="content-pdp-qnta__body">
+        <div class="content-pdp-qnta__body__left">
+          <div class="breadcrumb">
+            <ul>
+              <li>Inicio</li>
+              <li>Sao Paulo</li>
+              <li>Aclimacao</li>
+            </ul>
+          </div>
+        </div>
+        <div class="content-pdp-qnta__body__right">
+          <div class="content-pdp-qnta__body__right__sticky-box">
+            <h5>Imovel fresquino</h5>
+          </div>
+        </div>
+      </section>
+    </div>
 </template>
 
 <script>
