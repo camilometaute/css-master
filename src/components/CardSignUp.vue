2ff6c63f6d318587442dc0205fb7ecaaf443ad23
Camilo Metaute <camilometaute@lahaus.com>
2020-07-15 21:15:08 -0500

    markup card signup

--- src/components/CardSignUp.vue
+++ src/components/CardSignUp.vue
@@ -1,6 +1,45 @@
 <template>
   <section class="card-signup">
-    <h2>Hello I am card signup</h2>
+    <div class="card-signup--container">
+      <div class="card-signup--item">
+        <img src="https://image.flaticon.com/icons/svg/664/664468.svg" alt="">
+        <div class="card-signup--item--title">
+          <h2>Starter Licence</h2>
+          <span>You're finding your balance</span>
+        </div>
+        <div class="card-signup--item--price">
+          <h4>$10</h4>
+          <span>Per year</span>
+        </div>
+        <button>SIGN UP</button>
+      </div>
+
+      <div class="card-signup--item">
+        <img src="https://image.flaticon.com/icons/svg/664/664468.svg" alt="">
+        <div class="card-signup--item--title">
+          <h2>Starter Licence</h2>
+          <span>You're finding your balance</span>
+        </div>
+        <div class="card-signup--item--price">
+          <h4>$10</h4>
+          <span>Per year</span>
+        </div>
+        <button>SIGN UP</button>
+      </div>
+
+      <div class="card-signup--item">
+        <img src="https://image.flaticon.com/icons/svg/664/664468.svg" alt="">
+        <div class="card-signup--item--title">
+          <h2>Starter Licence</h2>
+          <span>You're finding your balance</span>
+        </div>
+        <div class="card-signup--item--price">
+          <h4>$10</h4>
+          <span>Per year</span>
+        </div>
+        <button>SIGN UP</button>
+      </div>
+    </div>
   </section>
 </template>
 
