9d33553f7dafb3247e5876bf27196984366f2223
Camilo Metaute <camilometaute@lahaus.com>
2020-07-15 20:49:36 -0500

    routes and demo view

---
+++ src/components/CardSignUp.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="card-signup">
+    <h2>Hello I am card signup</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'CardSignUp',
+  data: function() {
+    return {
+
+    }
+  }
+};
+</script>
