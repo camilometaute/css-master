a67ea5643161db2b0dc0abea24f1f703ac7c85a5
Camilo Metaute <camilometaute@lahaus.com>
2020-08-08 14:43:52 -0500

    skew effect with css in Y axis

--- src/components/StripeGrid.vue
+++ src/components/StripeGrid.vue
@@ -1,43 +1,51 @@
 <template>
-  <section class="stripe-grid">
-    <h3>Why Stripe</h3>
-    <h1>A technology-first approach to payments and finance</h1>
+  <section>
+      <div class="top-container">
 
-    <div class="stripe-grid__row">
-      <div class="stripe-grid__column">
-        <div class="stripe-grid__column-img">
-          <img src="https://image.flaticon.com/icons/svg/921/921510.svg" alt="">
+      </div>
+      <div class="stripe-grid">
+        <h3>Why Stripe</h3>
+        <h1>A technology-first approach to payments and finance</h1>
+
+        <div class="stripe-grid__row">
+          <div class="stripe-grid__column">
+            <div class="stripe-grid__column-img">
+              <img src="https://image.flaticon.com/icons/svg/921/921510.svg" alt="">
+            </div>
+            <h5 class="stripe-grid__column-title">Close metal</h5>
+            <p>From <span>direct integrations</span> with card networks and banks to checkout flows in the browser, we operate on and optimize at every level of the financial stack.</p>
+          </div>
+
+          <div class="stripe-grid__column">
+            <div class="stripe-grid__column-img">
+              <img src="https://image.flaticon.com/icons/svg/921/921510.svg" alt="">
+            </div>
+            <h5 class="stripe-grid__column-title">Fastest</h5>
+            <p>From <span>direct integrations</span> with card networks and banks to checkout flows in the browser, we operate on and optimize at every level of the financial stack.</p>
+          </div>
+
+          <div class="stripe-grid__column">
+            <div class="stripe-grid__column-img">
+              <img src="https://image.flaticon.com/icons/svg/921/921510.svg" alt="">
+            </div>
+            <h5 class="stripe-grid__column-title">Battle</h5>
+            <p>From <span>direct integrations</span> with card networks and banks to checkout flows in the browser, we operate on and optimize at every level of the financial stack.</p>
+          </div>
+
+          <div class="stripe-grid__column">
+            <div class="stripe-grid__column-img">
+              <img src="https://image.flaticon.com/icons/svg/921/921510.svg" alt="">
+            </div>
+            <h5 class="stripe-grid__column-title">Intelligent</h5>
+            <p>From <span>direct integrations</span> with card networks and banks to checkout flows in the browser, we operate on and optimize at every level of the financial stack.</p>
+          </div>
         </div>
-        <h5 class="stripe-grid__column-title">Close metal</h5>
-        <p>From <span>direct integrations</span> with card networks and banks to checkout flows in the browser, we operate on and optimize at every level of the financial stack.</p>
+      </div>
+      <div class="bottom-container">
+
       </div>
 
-      <div class="stripe-grid__column">
-        <div class="stripe-grid__column-img">
-          <img src="https://image.flaticon.com/icons/svg/921/921510.svg" alt="">
-        </div>
-        <h5 class="stripe-grid__column-title">Fastest</h5>
-        <p>From <span>direct integrations</span> with card networks and banks to checkout flows in the browser, we operate on and optimize at every level of the financial stack.</p>
-      </div>
-
-      <div class="stripe-grid__column">
-        <div class="stripe-grid__column-img">
-          <img src="https://image.flaticon.com/icons/svg/921/921510.svg" alt="">
-        </div>
-        <h5 class="stripe-grid__column-title">Battle</h5>
-        <p>From <span>direct integrations</span> with card networks and banks to checkout flows in the browser, we operate on and optimize at every level of the financial stack.</p>
-      </div>
-
-      <div class="stripe-grid__column">
-        <div class="stripe-grid__column-img">
-          <img src="https://image.flaticon.com/icons/svg/921/921510.svg" alt="">
-        </div>
-        <h5 class="stripe-grid__column-title">Intelligent</h5>
-        <p>From <span>direct integrations</span> with card networks and banks to checkout flows in the browser, we operate on and optimize at every level of the financial stack.</p>
-      </div>
-    </div>
   </section>
-
 </template>
 
 <script>
