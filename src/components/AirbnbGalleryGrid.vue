7d8c6a38a161fec38ec41292d31600636adcc590
Camilo Metaute <camilometaute@lahaus.com>
2020-09-19 07:54:52 -0500

    full markup with responsive grid gallery airbnb

---
+++ src/components/AirbnbGalleryGrid.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="airbnb-gallery-grid">
+    <div class="airbnb-gallery-grid__bg-column">
+      <img src="https://a0.muscache.com/im/pictures/1a4be255-f43f-441a-81d8-6d20485f1c0c.jpg?im_w=960" alt="">
+    </div>
+    <div class="airbnb-gallery-grid__sm-column">
+      <img src="https://a0.muscache.com/im/pictures/e1d0f271-674b-4576-9774-87b54ce7979d.jpg?im_w=720" alt="">
+      <img src="https://a0.muscache.com/im/pictures/b64356f4-98cc-40da-a922-a188ef72f4b9.jpg?im_w=720" alt="">
+    </div>
+    <div class="airbnb-gallery-grid__sm-column">
+      <img src="https://a0.muscache.com/im/pictures/607d70db-d876-4087-b1e9-0cb97bd2a15d.jpg?im_w=720" alt="">
+      <img src="https://a0.muscache.com/im/pictures/04a9bbe2-d1ec-4c75-b18b-bff21baacb63.jpg?im_w=720" alt="">
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AirbnbGalleryGrid'
+};
+</script>
