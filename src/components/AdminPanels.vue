74f71457e861a8ece91e752290ed19af1f6fe43e
Camilo Metaute <camilometaute@lahaus.com>
2020-10-24 20:12:02 -0500

    markup first panel

---
+++ src/components/AdminPanels.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="admin-panels">
+    <div class="admin-panels__columns panel-1">
+      <div class="admin-panels__columns__user">
+        <img src="https://designreset.com/cork/ltr/demo3/assets/img/profile-32.jpeg" alt="">
+        <div class="admin-panels__columns__user__name">
+          <h4>Jimmy Turner</h4>
+          <span>Monday, Nov 18</span>
+        </div>
+      </div>
+      <hr>
+      <div class="admin-panels__columns__info">
+        <p>"Duis aute irure dolor" in reprehenderit in voluptate velit esse cillum "dolore eu fugiat" nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
+        <div class="admin-panels__columns__info__likes">
+          <font-awesome-icon icon="clock" class="overview__icon-star" /></i>
+          <span>551 Likes</span>
+        </div>
+      </div>
+    </div>
+    <div class="admin-panels__columns panel-2">
+      <h3>Panel Title</h3>
+    </div>
+    <div class="admin-panels__columns panel-3">
+      <h3>Panel Title</h3>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AdminPanels',
+  data: function() {
+    return {
+      titlePanel: "Jimmy Turner"
+    }
+  }
+};
+</script>
