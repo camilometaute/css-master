c4e76d525706e2155d183880e32153b10af5fe8d
Camilo Metaute <camilometaute@lahaus.com>
2021-01-27 19:23:59 -0500

    dark mode from cookies component

--- src/components/CookiesComponents.vue
+++ src/components/CookiesComponents.vue
@@ -8,30 +8,15 @@
           <button type="button" name="button" class="allow">Allow</button>
           <button type="button" name="button" class="decline">Decline</button>
         </div>
-      <!-- </div>
-      <div class="cookie-light-sm">
-        <font-awesome-icon icon="cookie-bite" class="overview__icon-star" /></i>
-        <p>We use thir-party <a href="#">cookies</a> in order to personalize your site expierience.</p>
-        <div class="cookie-light-sm__btn-container">
-          <button type="button" name="button">Allow</button>
-          <button type="button" name="button">Decline</button>
-        </div>
       </div>
+
       <div class="cookie-dark-xl">
         <font-awesome-icon icon="cookie-bite" class="overview__icon-star" /></i>
         <p>We use thir-party <a href="#">cookies</a> in order to personalize your site expierience.</p>
         <div class="cookie-dark-xl__btn-container">
-          <button type="button" name="button">Allow</button>
-          <button type="button" name="button">Decline</button>
+          <button type="button" name="button" class="allow">Allow</button>
+          <button type="button" name="button" class="decline">Decline</button>
         </div>
-      </div>
-      <div class="cookie-dark-md">
-        <font-awesome-icon icon="cookie-bite" class="overview__icon-star" /></i>
-        <p>We use thir-party <a href="#">cookies</a> in order to personalize your site expierience.</p>
-        <div class="cookie-dark-md__btn-container">
-          <button type="button" name="button">Allow</button>
-          <button type="button" name="button">Decline</button>
-        </div> -->
       </div>
     </div>
   </section>
