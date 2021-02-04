1f65c41cff2362160e08275d4641fb39e811ffad
Camilo Metaute <camilometaute@lahaus.com>
2021-02-03 20:54:16 -0500

    list top products with markup and logic js

--- src/App.vue
+++ src/App.vue
@@ -7,55 +7,56 @@
 
       <div class="menu__options">
         <span>Pages</span>
-        <router-link to="/outdoor"><font-awesome-icon icon="mobile" /></i>OutDoor Project</router-link>
-        <router-link to="/flexbox-master"><font-awesome-icon icon="mobile" /></i>Flex Box Master</router-link>
+        <router-link to="/outdoor"><font-awesome-icon icon="mobile" />OutDoor Project</router-link>
+        <router-link to="/flexbox-master"><font-awesome-icon icon="mobile" />Flex Box Master</router-link>
         <span>Practice</span>
-        <router-link to="/"><font-awesome-icon icon="map" /></i>Computed & Vuejs</router-link>
-        <router-link to="/css-grid-basis"><font-awesome-icon icon="map" /></i>CSS grid basis</router-link>
-        <router-link to="/website-css-grid"><font-awesome-icon icon="map" /></i>Web css grid</router-link>
-        <router-link to="/scroll-handler-section"><font-awesome-icon icon="map" /></i>Scroll Handler Section</router-link>
+        <router-link to="/"><font-awesome-icon icon="map" />Computed & Vuejs</router-link>
+        <router-link to="/css-grid-basis"><font-awesome-icon icon="map" />CSS grid basis</router-link>
+        <router-link to="/website-css-grid"><font-awesome-icon icon="map" />Web css grid</router-link>
+        <router-link to="/scroll-handler-section"><font-awesome-icon icon="map" />Scroll Handler Section</router-link>
         <span>¿Can I do this?</span>
-        <router-link to="/trulia-cards"><font-awesome-icon icon="bookmark" /></i>Trulia cards</router-link>
-        <router-link to="/openlisting-cards"><font-awesome-icon icon="bookmark" /></i>Open Listing Cards</router-link>
-        <router-link to="/grammarly-section"><font-awesome-icon icon="bookmark" /></i>Grammarly Section</router-link>
-        <router-link to="/opendoor-adviser"><font-awesome-icon icon="bookmark" /></i>OpenDoor Adviser</router-link>
-        <router-link to="/airbnb-cards"><font-awesome-icon icon="bookmark" /></i>Airbnb Cards</router-link>
-        <router-link to="/contact-opendoor"><font-awesome-icon icon="bookmark" /></i>Contact OpenDoor</router-link>
-        <router-link to="/airbnb-header-section"><font-awesome-icon icon="bookmark" /></i>Airbnb Header</router-link>
-        <router-link to="/opendoor-pic-section"><font-awesome-icon icon="bookmark" /></i>Open Door Pic Section</router-link>
-        <router-link to="/srp-open-door"><font-awesome-icon icon="bookmark" /></i>SRP OpenDoor</router-link>
-        <router-link to="/tabs-stripe"><font-awesome-icon icon="bookmark" /></i>Tabs stripe</router-link>
-        <router-link to="/grid-gallery-columns"><font-awesome-icon icon="bookmark" /></i>Grid gallery columns</router-link>
-        <router-link to="/airbnb-grid"><font-awesome-icon icon="bookmark" /></i>Airbnb Grid</router-link>
-        <router-link to="/compass-section"><font-awesome-icon icon="bookmark" /></i>Compass Section</router-link>
-        <router-link to="/quindoandar-client-section"><font-awesome-icon icon="bookmark" /></i>Qto An client section</router-link>
-        <router-link to="/karim-section"><font-awesome-icon icon="bookmark" /></i>Karim Section</router-link>
-        <router-link to="/stripe-carousel"><font-awesome-icon icon="bookmark" /></i>Stripe carousel</router-link>
-        <router-link to="/cards-zillow"><font-awesome-icon icon="bookmark" /></i>Cards Zillow</router-link>
-        <router-link to="/pdp-quintoandar"><font-awesome-icon icon="bookmark" /></i>PDP quintoandar</router-link>
-        <router-link to="/list-payment-history"><font-awesome-icon icon="bookmark" /></i>List payment history</router-link>
-        <router-link to="/casper-grid-section"><font-awesome-icon icon="bookmark" /></i>Casper grid section</router-link>
-        <router-link to="/card-signup"><font-awesome-icon icon="bookmark" /></i>Card Sign Up</router-link>
-        <router-link to="/donuts-calculator"><font-awesome-icon icon="bookmark" /></i>Donuts calculator</router-link>
-        <router-link to="/opendoor-comparative-box"><font-awesome-icon icon="bookmark" /></i>Opendoor Comparative</router-link>
-        <router-link to="/dashboard-grid"><font-awesome-icon icon="bookmark" /></i>Dashboard grid</router-link>
-        <router-link to="/stripe-grid"><font-awesome-icon icon="bookmark" /></i>Stripe grid section</router-link>
-        <router-link to="/stripe-customer-card"><font-awesome-icon icon="bookmark" /></i>Stripe Customer card</router-link>
-        <router-link to="/admin-layout"><font-awesome-icon icon="bookmark" /></i>Admin layout</router-link>
-        <router-link to="/card-ref-ui"><font-awesome-icon icon="bookmark" /></i>Card Refactoring UI</router-link>
-        <router-link to="/airbnb-gallery-grid"><font-awesome-icon icon="bookmark" /></i>Airbnb Gallery grid</router-link>
-        <router-link to="/admin-grid-transactions"><font-awesome-icon icon="bookmark" /></i>Admin grid transactions</router-link>
-        <router-link to="/guglieri-sticky"><font-awesome-icon icon="bookmark" /></i>Guglieri sticky</router-link>
-        <router-link to="/mail-inbox"><font-awesome-icon icon="bookmark" /></i>Mail Inbox</router-link>
-        <router-link to="/admin-panels"><font-awesome-icon icon="bookmark" /></i>Admin Panels</router-link>
-        <router-link to="/sealco-grid"><font-awesome-icon icon="bookmark" /></i>Dinamic Grid Sealco</router-link>
-        <router-link to="/accent-box-hover"><font-awesome-icon icon="bookmark" /></i>Accent Box Hover</router-link>
-        <router-link to="/accent-products-section"><font-awesome-icon icon="bookmark" /></i>Accent products section</router-link>
-        <router-link to="/navbar-3-ways"><font-awesome-icon icon="bookmark" /></i>Navbar 3 ways</router-link>
-        <router-link to="/tubik-company"><font-awesome-icon icon="bookmark" /></i>Tubik company section</router-link>
-        <router-link to="/landing-user-interface"><font-awesome-icon icon="bookmark" /></i>Landing User Interface</router-link>
-        <router-link to="/card-face"><font-awesome-icon icon="bookmark" /></i>Card face</router-link>
-        <router-link to="/cookies-components"><font-awesome-icon icon="bookmark" /></i>Cookies components</router-link>
+        <router-link to="/trulia-cards"><font-awesome-icon icon="bookmark" />Trulia cards</router-link>
+        <router-link to="/openlisting-cards"><font-awesome-icon icon="bookmark" />Open Listing Cards</router-link>
+        <router-link to="/grammarly-section"><font-awesome-icon icon="bookmark" />Grammarly Section</router-link>
+        <router-link to="/opendoor-adviser"><font-awesome-icon icon="bookmark" />OpenDoor Adviser</router-link>
+        <router-link to="/airbnb-cards"><font-awesome-icon icon="bookmark" />Airbnb Cards</router-link>
+        <router-link to="/contact-opendoor"><font-awesome-icon icon="bookmark" />Contact OpenDoor</router-link>
+        <router-link to="/airbnb-header-section"><font-awesome-icon icon="bookmark" />Airbnb Header</router-link>
+        <router-link to="/opendoor-pic-section"><font-awesome-icon icon="bookmark" />Open Door Pic Section</router-link>
+        <router-link to="/srp-open-door"><font-awesome-icon icon="bookmark" />SRP OpenDoor</router-link>
+        <router-link to="/tabs-stripe"><font-awesome-icon icon="bookmark" />Tabs stripe</router-link>
+        <router-link to="/grid-gallery-columns"><font-awesome-icon icon="bookmark" />Grid gallery columns</router-link>
+        <router-link to="/airbnb-grid"><font-awesome-icon icon="bookmark" />Airbnb Grid</router-link>
+        <router-link to="/compass-section"><font-awesome-icon icon="bookmark" />Compass Section</router-link>
+        <router-link to="/quindoandar-client-section"><font-awesome-icon icon="bookmark" />Qto An client section</router-link>
+        <router-link to="/karim-section"><font-awesome-icon icon="bookmark" />Karim Section</router-link>
+        <router-link to="/stripe-carousel"><font-awesome-icon icon="bookmark" />Stripe carousel</router-link>
+        <router-link to="/cards-zillow"><font-awesome-icon icon="bookmark" />Cards Zillow</router-link>
+        <router-link to="/pdp-quintoandar"><font-awesome-icon icon="bookmark" />PDP quintoandar</router-link>
+        <router-link to="/list-payment-history"><font-awesome-icon icon="bookmark" />List payment history</router-link>
+        <router-link to="/casper-grid-section"><font-awesome-icon icon="bookmark" />Casper grid section</router-link>
+        <router-link to="/card-signup"><font-awesome-icon icon="bookmark" />Card Sign Up</router-link>
+        <router-link to="/donuts-calculator"><font-awesome-icon icon="bookmark" />Donuts calculator</router-link>
+        <router-link to="/opendoor-comparative-box"><font-awesome-icon icon="bookmark" />Opendoor Comparative</router-link>
+        <router-link to="/dashboard-grid"><font-awesome-icon icon="bookmark" />Dashboard grid</router-link>
+        <router-link to="/stripe-grid"><font-awesome-icon icon="bookmark" />Stripe grid section</router-link>
+        <router-link to="/stripe-customer-card"><font-awesome-icon icon="bookmark" />Stripe Customer card</router-link>
+        <router-link to="/admin-layout"><font-awesome-icon icon="bookmark" />Admin layout</router-link>
+        <router-link to="/card-ref-ui"><font-awesome-icon icon="bookmark" />Card Refactoring UI</router-link>
+        <router-link to="/airbnb-gallery-grid"><font-awesome-icon icon="bookmark" />Airbnb Gallery grid</router-link>
+        <router-link to="/admin-grid-transactions"><font-awesome-icon icon="bookmark" />Admin grid transactions</router-link>
+        <router-link to="/guglieri-sticky"><font-awesome-icon icon="bookmark" />Guglieri sticky</router-link>
+        <router-link to="/mail-inbox"><font-awesome-icon icon="bookmark" />Mail Inbox</router-link>
+        <router-link to="/admin-panels"><font-awesome-icon icon="bookmark" />Admin Panels</router-link>
+        <router-link to="/sealco-grid"><font-awesome-icon icon="bookmark" />Dinamic Grid Sealco</router-link>
+        <router-link to="/accent-box-hover"><font-awesome-icon icon="bookmark" />Accent Box Hover</router-link>
+        <router-link to="/accent-products-section"><font-awesome-icon icon="bookmark" />Accent products section</router-link>
+        <router-link to="/navbar-3-ways"><font-awesome-icon icon="bookmark" />Navbar 3 ways</router-link>
+        <router-link to="/tubik-company"><font-awesome-icon icon="bookmark" />Tubik company section</router-link>
+        <router-link to="/landing-user-interface"><font-awesome-icon icon="bookmark" />Landing User Interface</router-link>
+        <router-link to="/card-face"><font-awesome-icon icon="bookmark" />Card face</router-link>
+        <router-link to="/cookies-components"><font-awesome-icon icon="bookmark" />Cookies components</router-link>
+        <router-link to="/top-product-sales"><font-awesome-icon icon="bookmark" />Top product sales</router-link>
       </div>
     </div>
     <router-view/>
--- src/router.js
+++ src/router.js
@@ -1,292 +1,298 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import TruliaCards from './components/TruliaCards.vue'
-import OpenListingCards from './components/OpenListingCards.vue'
-import OutDoorProject from './components/OutDoorProject.vue'
-import WebSiteCssGrid from './components/WebSiteCssGrid.vue'
-import CssGridBasis from './components/CssGridBasis.vue'
-import GrammarlySection from './components/GrammarlySection.vue'
-import FlexBoxMaster from './components/FlexBoxMaster.vue'
-import OpenDoorAdviser from './components/OpendoorAdviser.vue'
-import AirbnbCards from './components/AirbnbCards.vue'
-import ContactOpenDoor from './components/ContactOpenDoor.vue'
-import AirbnbHeadSection from './components/AirbnbHeadSection.vue'
-import OpenDoorPictureSection from './components/OpenDoorPictureSection.vue'
-import ScrollHandlerSections from './components/ScrollHandlerSections.vue'
-import SrpOpenDoor from './components/SrpOpenDoor.vue'
-import TabsStripe from './components/TabsStripe.vue'
-import GridGalleryColumns from './components/GridGalleryColumns.vue'
-import AirbnbGrid from './components/AirbnbGrid.vue'
-import CompasSection from './components/CompasSection.vue'
-import QuintoAndarClientSection from './components/QuintoAndarClientSection.vue'
-import KarimSection from './components/KarimSection.vue'
-import StripeCarousel from './components/StripeCarousel.vue'
-import CardsZillow from './components/CardsZillow.vue'
-import PdpQuintoAndar from './components/PdpQuintoAndar.vue'
-import ListPaymentHistory from './components/ListPaymentHistory.vue'
-import CasperGridSection from './components/CasperGridSection.vue'
-import CardSignUp from './components/CardSignUp.vue'
-import DonutsCalculator from './components/DonutsCalculator.vue'
-import OpenDoorComparativeBox from './components/OpenDoorComparativeBox.vue'
-import DashboardGrid from './components/DashboardGrid.vue'
-import StripeGrid from './components/StripeGrid.vue'
-import StripeCustomerCard from './components/StripeCustomerCard.vue'
-import AdminLayout from './components/AdminLayout.vue'
-import CardRefUi from './components/CardRefUi.vue'
-import AirbnbGalleryGrid from './components/AirbnbGalleryGrid.vue'
-import AdminGridTransactions from './components/AdminGridTransactions.vue'
-import GuglieriSticky from './components/GuglieriSticky.vue'
-import MailInbox from './components/MailInbox.vue'
-import AdminPanels from './components/AdminPanels.vue'
-import SealcoGrid from './components/SealcoGrid.vue'
-import AccentBoxHover from './components/AccentBoxHover.vue'
-import AccentProductSection from './components/AccentProductSection.vue'
-import Navbar3Ways from './components/Navbar3Ways.vue'
-import TubikCompany from './components/TubikCompany.vue'
-import LandingUserInterface from './components/LandingUserInterface.vue'
-import CardFace from './components/CardFace.vue'
-import CookiesComponents from './components/CookiesComponents.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
+import TruliaCards from './components/TruliaCards.vue';
+import OpenListingCards from './components/OpenListingCards.vue';
+import OutDoorProject from './components/OutDoorProject.vue';
+import WebSiteCssGrid from './components/WebSiteCssGrid.vue';
+import CssGridBasis from './components/CssGridBasis.vue';
+import GrammarlySection from './components/GrammarlySection.vue';
+import FlexBoxMaster from './components/FlexBoxMaster.vue';
+import OpenDoorAdviser from './components/OpendoorAdviser.vue';
+import AirbnbCards from './components/AirbnbCards.vue';
+import ContactOpenDoor from './components/ContactOpenDoor.vue';
+import AirbnbHeadSection from './components/AirbnbHeadSection.vue';
+import OpenDoorPictureSection from './components/OpenDoorPictureSection.vue';
+import ScrollHandlerSections from './components/ScrollHandlerSections.vue';
+import SrpOpenDoor from './components/SrpOpenDoor.vue';
+import TabsStripe from './components/TabsStripe.vue';
+import GridGalleryColumns from './components/GridGalleryColumns.vue';
+import AirbnbGrid from './components/AirbnbGrid.vue';
+import CompasSection from './components/CompasSection.vue';
+import QuintoAndarClientSection from './components/QuintoAndarClientSection.vue';
+import KarimSection from './components/KarimSection.vue';
+import StripeCarousel from './components/StripeCarousel.vue';
+import CardsZillow from './components/CardsZillow.vue';
+import PdpQuintoAndar from './components/PdpQuintoAndar.vue';
+import ListPaymentHistory from './components/ListPaymentHistory.vue';
+import CasperGridSection from './components/CasperGridSection.vue';
+import CardSignUp from './components/CardSignUp.vue';
+import DonutsCalculator from './components/DonutsCalculator.vue';
+import OpenDoorComparativeBox from './components/OpenDoorComparativeBox.vue';
+import DashboardGrid from './components/DashboardGrid.vue';
+import StripeGrid from './components/StripeGrid.vue';
+import StripeCustomerCard from './components/StripeCustomerCard.vue';
+import AdminLayout from './components/AdminLayout.vue';
+import CardRefUi from './components/CardRefUi.vue';
+import AirbnbGalleryGrid from './components/AirbnbGalleryGrid.vue';
+import AdminGridTransactions from './components/AdminGridTransactions.vue';
+import GuglieriSticky from './components/GuglieriSticky.vue';
+import MailInbox from './components/MailInbox.vue';
+import AdminPanels from './components/AdminPanels.vue';
+import SealcoGrid from './components/SealcoGrid.vue';
+import AccentBoxHover from './components/AccentBoxHover.vue';
+import AccentProductSection from './components/AccentProductSection.vue';
+import Navbar3Ways from './components/Navbar3Ways.vue';
+import TubikCompany from './components/TubikCompany.vue';
+import LandingUserInterface from './components/LandingUserInterface.vue';
+import CardFace from './components/CardFace.vue';
+import CookiesComponents from './components/CookiesComponents.vue';
+import TopProductSales from './components/TopProductSales.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router ({
+export default new Router({
   routes: [
 
     {
       path: '/',
       name: 'helloworld',
-      component: HelloWorld
+      component: HelloWorld,
     },
     {
       path: '/trulia-cards',
       name: 'TruliaCards',
-      component: TruliaCards
+      component: TruliaCards,
     },
     {
       path: '/css-grid-basis',
       name: 'CssGridBasis',
-      component: CssGridBasis
+      component: CssGridBasis,
     },
     {
       path: '/openlisting-cards',
       name: 'OpenListingCards',
-      component: OpenListingCards
+      component: OpenListingCards,
     },
     {
       path: '/outdoor',
       name: 'OutDoorProject',
-      component: OutDoorProject
+      component: OutDoorProject,
     },
     {
       path: '/website-css-grid',
       name: 'WebSiteCssGrid',
-      component: WebSiteCssGrid
+      component: WebSiteCssGrid,
     },
     {
       path: '/grammarly-section',
       name: 'GrammarlySection',
-      component: GrammarlySection
+      component: GrammarlySection,
     },
     {
       path: '/flexbox-master',
       name: 'FlexBoxMaster',
-      component: FlexBoxMaster
+      component: FlexBoxMaster,
     },
     {
       path: '/opendoor-adviser',
       name: 'OpenDoorAdviser',
-      component: OpenDoorAdviser
+      component: OpenDoorAdviser,
     },
     {
       path: '/airbnb-cards',
       name: 'AirbnbCards',
-      component: AirbnbCards
+      component: AirbnbCards,
     },
     {
       path: '/contact-opendoor',
       name: 'ContactOpenDoor',
-      component: ContactOpenDoor
+      component: ContactOpenDoor,
     },
     {
       path: '/airbnb-header-section',
       name: 'AirbnbHeadSection',
-      component: AirbnbHeadSection
+      component: AirbnbHeadSection,
     },
     {
       path: '/opendoor-pic-section',
       name: 'OpenDoorPictureSection',
-      component: OpenDoorPictureSection
+      component: OpenDoorPictureSection,
     },
     {
       path: '/scroll-handler-section',
       name: 'ScrollHandlerSections',
-      component: ScrollHandlerSections
+      component: ScrollHandlerSections,
     },
     {
       path: '/srp-open-door',
       name: 'SrpOpenDoor',
-      component: SrpOpenDoor
+      component: SrpOpenDoor,
     },
     {
       path: '/tabs-stripe',
       name: 'TabsStripe',
-      component: TabsStripe
+      component: TabsStripe,
     },
     {
       path: '/grid-gallery-columns',
       name: 'GridGalleryColumns',
-      component: GridGalleryColumns
+      component: GridGalleryColumns,
     },
     {
       path: '/airbnb-grid',
       name: 'AirbnbGrid',
-      component: AirbnbGrid
+      component: AirbnbGrid,
     },
     {
       path: '/compass-section',
       name: 'CompasSection',
-      component: CompasSection
+      component: CompasSection,
     },
     {
       path: '/quindoandar-client-section',
       name: 'QuintoAndarClientSection',
-      component: QuintoAndarClientSection
+      component: QuintoAndarClientSection,
     },
     {
       path: '/karim-section',
       name: 'KarimSection',
-      component: KarimSection
+      component: KarimSection,
     },
     {
       path: '/stripe-carousel',
       name: 'StripeCarousel',
-      component: StripeCarousel
+      component: StripeCarousel,
     },
     {
       path: '/cards-zillow',
       name: 'CardsZillow',
-      component: CardsZillow
+      component: CardsZillow,
     },
     {
       path: '/pdp-quintoandar',
       name: 'PdpQuintoAndar',
-      component: PdpQuintoAndar
+      component: PdpQuintoAndar,
     },
     {
       path: '/list-payment-history',
       name: 'ListPaymentHistory',
-      component: ListPaymentHistory
+      component: ListPaymentHistory,
     },
     {
       path: '/casper-grid-section',
       name: 'CasperGridSection',
-      component: CasperGridSection
+      component: CasperGridSection,
     },
     {
       path: '/card-signup',
       name: 'CardSignUp',
-      component: CardSignUp
+      component: CardSignUp,
     },
     {
       path: '/donuts-calculator',
       name: 'DonutsCalculator',
-      component: DonutsCalculator
+      component: DonutsCalculator,
     },
     {
       path: '/opendoor-comparative-box',
       name: 'OpenDoorComparativeBox',
-      component: OpenDoorComparativeBox
+      component: OpenDoorComparativeBox,
     },
     {
       path: '/dashboard-grid',
       name: 'DashboardGrid',
-      component: DashboardGrid
+      component: DashboardGrid,
     },
     {
       path: '/stripe-grid',
       name: 'StripeGrid',
-      component: StripeGrid
+      component: StripeGrid,
     },
     {
       path: '/stripe-customer-card',
       name: 'StripeCustomerCard',
-      component: StripeCustomerCard
+      component: StripeCustomerCard,
     },
     {
       path: '/admin-layout',
       name: 'AdminLayout',
-      component: AdminLayout
+      component: AdminLayout,
     },
     {
       path: '/card-ref-ui',
       name: 'CardRefUi',
-      component: CardRefUi
+      component: CardRefUi,
     },
     {
       path: '/airbnb-gallery-grid',
       name: 'AirbnbGalleryGrid',
-      component: AirbnbGalleryGrid
+      component: AirbnbGalleryGrid,
     },
     {
       path: '/admin-grid-transactions',
       name: 'AdminGridTransactions',
-      component: AdminGridTransactions
+      component: AdminGridTransactions,
     },
     {
       path: '/guglieri-sticky',
       name: 'GuglieriSticky',
-      component: GuglieriSticky
+      component: GuglieriSticky,
     },
     {
       path: '/mail-inbox',
       name: 'MailInbox',
-      component: MailInbox
+      component: MailInbox,
     },
     {
       path: '/admin-panels',
       name: 'AdminPanels',
-      component: AdminPanels
+      component: AdminPanels,
     },
     {
       path: '/sealco-grid',
       name: 'SealcoGrid',
-      component: SealcoGrid
+      component: SealcoGrid,
     },
     {
       path: '/accent-box-hover',
       name: 'AccentBoxHover',
-      component: AccentBoxHover
+      component: AccentBoxHover,
     },
     {
       path: '/accent-products-section',
       name: 'AccentProductSection',
-      component: AccentProductSection
+      component: AccentProductSection,
     },
     {
       path: '/navbar-3-ways',
       name: 'Navbar3Ways',
-      component: Navbar3Ways
+      component: Navbar3Ways,
     },
     {
       path: '/tubik-company',
       name: 'TubikCompany',
-      component: TubikCompany
+      component: TubikCompany,
     },
     {
       path: '/landing-user-interface',
       name: 'LandingUserInterface',
-      component: LandingUserInterface
+      component: LandingUserInterface,
     },
     {
       path: '/card-face',
       name: 'CardFace',
-      component: CardFace
+      component: CardFace,
     },
     {
       path: '/cookies-components',
       name: 'CookiesComponents',
-      component: CookiesComponents
-    }
-  ]
-})
+      component: CookiesComponents,
+    },
+    {
+      path: '/top-product-sales',
+      name: 'TopProductSales',
+      component: TopProductSales,
+    },
+  ],
+});
