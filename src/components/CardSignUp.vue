c222ec63b0057e12389a5e54ad2a73a3e6f6a985
Camilo Metaute <camilometaute@lahaus.com>
2021-05-17 08:44:51 -0500

    variables with sass until cookies component

--- src/components/CardSignUp.vue
+++ src/components/CardSignUp.vue
@@ -2,7 +2,7 @@
   <section class="card-signup">
     <div class="card-signup--container">
       <div class="card-signup--item">
-        <img src="https://image.flaticon.com/icons/svg/664/664468.svg" alt="">
+        <img src="https://img-premium.flaticon.com/png/512/776/776588.png?token=exp=1621258186~hmac=b79c5229cb138c0d74432ca75dbdd931" alt="">
         <div class="card-signup--item--title">
           <h2>Starter Licence</h2>
           <span>You're finding your balance</span>
@@ -15,7 +15,7 @@
       </div>
 
       <div class="card-signup--item">
-        <img src="https://image.flaticon.com/icons/svg/664/664468.svg" alt="">
+        <img src="https://img-premium.flaticon.com/png/512/776/776588.png?token=exp=1621258186~hmac=b79c5229cb138c0d74432ca75dbdd931" alt="">
         <div class="card-signup--item--title">
           <h2>Starter Licence</h2>
           <span>You're finding your balance</span>
@@ -28,7 +28,7 @@
       </div>
 
       <div class="card-signup--item">
-        <img src="https://image.flaticon.com/icons/svg/664/664468.svg" alt="">
+        <img src="https://img-premium.flaticon.com/png/512/776/776588.png?token=exp=1621258186~hmac=b79c5229cb138c0d74432ca75dbdd931" alt="">
         <div class="card-signup--item--title">
           <h2>Starter Licence</h2>
           <span>You're finding your balance</span>
