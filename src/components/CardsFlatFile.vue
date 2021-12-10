74c2791eee1421d23f249ea228b432b0720f8f89
Camilo Metaute <milometaute@gmail.com>
2021-12-09 21:18:46 -0500

    cards flat file markup

---
+++ src/components/CardsFlatFile.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flat-file">
+    <h2>Many teams, one solution.</h2>
+    <div class="row-cards">
+      <div class="card card-blue">
+        <img src="https://flatfile.com/img/icons/circle-square-overlap.svg" alt="">
+        <h4>Product & engineering</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <span>Never wait on the ingetration being buil "next quarter"</span>
+      </div>
+      <div class="card card-yellow">
+        <img src="https://flatfile.com/img/icons/shaded-squares.svg" alt="">
+        <h4>Success & implementations</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua with this and another this.</p>
+        <span>Never wait on the ingetration being buil "next quarter"</span>
+      </div>
+      <div class="card card-salmon">
+        <img src="https://flatfile.com/img/icons/overlapping-circles.svg" alt="">
+        <h4>Sales & marketing</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <span>Never wait on the ingetration being buil "next quarter"</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FlatFile'
+};
+</script>
