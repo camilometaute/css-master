64bcc398a23358327ad85e9662cb85439ef30a88
Camilo Metaute <milometaute@gmail.com>
2021-12-19 20:22:39 -0500

    markup cards speedy

---
+++ src/components/speedySection.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="speedy-section">
+    <div class="section-1"></div>
+    <div class="section-2">
+      <div class="card card--1">
+        <h3>Integration Support</h3>
+        <p>Your time is your business. We provide human support throughout the integration process for quick onboarding.</p>
+      </div>
+      <div class="card card--2">
+        <h3>Local & Global Payouts</h3>
+        <p>Your time is your business. We provide human support throughout the integration process for quick onboarding.</p>
+      </div>
+      <div class="card card--3">
+        <h3>Technology That Scales</h3>
+        <p>Speedy’s payments API is built to scale as your business grows without comprimising from speed or security.</p>
+      </div>
+    </div>
+    <div class="section-3"></div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AdminPanels',
+  data: function() {
+    return {
+      titlePanel: "Jimmy Turner"
+    }
+  }
+};
+</script>
