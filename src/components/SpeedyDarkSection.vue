cd04016a5b996a442d15de0cc26f678dc238c099
Camilo Metaute <milometaute@gmail.com>
2021-12-25 21:49:44 -0500

    markup dark section

---
+++ src/components/SpeedyDarkSection.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="speedy-dark-section">
+    <ul>
+      <li>
+        <hr>
+        <span>
+          <strong>Easy Integration.</strong>
+          Accept payments in minutes with our simple and scalable API.
+        </span>
+      </li>
+      <li>
+        <hr>
+        <span>
+          <strong>Global Payment Methods.</strong>
+           30+ payment methods, covering your region and beyond.
+        </span>
+      </li>
+      <li>
+        <hr>
+        <span>
+          <strong>Lowest Fees.</strong>
+           Lowest transaction fees. No recurring subscription or setup costs.
+        </span>
+      </li>
+    </ul>
+    <div class="payment-section">
+      <h1>Create on-brand checkout experiences with multiple payment options easily.</h1>
+      <div class="payment-section__img">
+        <img class="payment-section__img__front" src="https://cdn.sanity.io/images/1ib26v3b/new/4d159fc04712ea6f9283ddaef5b4ba500f6d6799-750x1008.png?auto=format">
+        <img class="payment-section__img__back" src="https://cdn.sanity.io/images/1ib26v3b/new/2d07b274ab154f5bad3cc58ebe65add42647f9ed-2828x1734.jpg?auto=format">
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SpeedyDarkSection',
+};
+</script>
