c7c0583ebc36c05e6257ff807e2e5012cb37dc6d
Camilo Metaute <milometaute@gmail.com>
2022-03-03 22:01:00 -0500

    markup section affirm

---
+++ src/components/AffirmSection.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="affirm-section">
+    <div class="affirm-section__left">
+      <img src="https://images.ctfassets.net/4rc1asww3mw7/70kjj6ISPr9Qie98Q5fFeo/3dedf7ee090a38cc7845d9afefdf13b5/The_choice_is_clear_Desktop_2x.jpg?w=912&h=961&q=50&fm=webp" alt="">
+    </div>
+    <div class="affirm-section__right">
+      <h2>The choice is clear</h2>
+      <p>Late fees? Hidden charges? Sneaky fine print? No thanks, we’ll leave those to the credit card companies. At Affirm, we like to keep it real—and that means no fees, no gotchas, and no regrets.</p>
+      <div class="actions">
+        <button class="primary">Get prequalified</button>
+        <button class="secondary">Learn more</button>
+      </div>
+      <span>This eligibility check won't affect your credit score.</span>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'AirbnbCards'
+};
+</script>
