352ed620cbaab8484a675c92e12089fab77a1848
Camilo Metaute <milometaute@gmail.com>
2022-03-20 23:08:44 -0500

    hover effect without fontawesome :(

---
+++ src/components/ButtonsEffects.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="buttons-effects">
+    <button class="button-return">RETURN</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonsEffects'
+};
+</script>
