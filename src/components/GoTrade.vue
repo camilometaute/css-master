d3f0f1abb216d6cec41f789b0811865d6e01f524
Camilo Metaute <milometaute@gmail.com>
2023-06-03 16:59:57 -0500

    grid layout created

--- src/components/GoTrade.vue
+++ src/components/GoTrade.vue
@@ -1,6 +1,20 @@
 <template>
   <section class="go-trade-container">
     <h1>Read our interesting articles about investments</h1>
+    <div class="go-trade-container__grid">
+      <div class="go-trade-container__grid__1">
+
+      </div>
+      <div class="go-trade-container__grid__2">
+
+      </div>
+      <div class="go-trade-container__grid__3">
+
+      </div>
+      <div class="go-trade-container__grid__4">
+
+      </div>
+    </div>
   </section>
 </template>
 
