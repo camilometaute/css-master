87913f55f709c8fdd4b12cdd453546e212c5609e
Camilo Metaute <milometaute@gmail.com>
2023-06-03 18:34:55 -0500

    cards design with images and before elements

--- src/components/GoTrade.vue
+++ src/components/GoTrade.vue
@@ -2,17 +2,45 @@
   <section class="go-trade-container">
     <h1>Read our interesting articles about investments</h1>
     <div class="go-trade-container__grid">
+
       <div class="go-trade-container__grid__1">
-
+        <img class="go-trade-container__grid__1__img" src="https://assets.website-files.com/61f2a069c6678de4aa2afb91/61f2c2aa199692e6ba4cd46a_read_ava.jpg" alt="">
+        <span>Explore expert videos</span>
+        <h3>User benefits of the Investments Personal 360°</h3>
+        <div class="go-trade-container__grid__1__actions">
+          <p>Play video for more</p>
+          <button type="button" name="button"><font-awesome-icon icon="arrow-right" /></i></button>
+        </div>
       </div>
+
       <div class="go-trade-container__grid__2">
-
+        <img class="go-trade-container__grid__2__img-1" src="https://assets.website-files.com/61f2a069c6678de4aa2afb91/61f7e8b6e06e346fc817423b_articles_stat-date.svg" alt="">
+        <img class="go-trade-container__grid__2__img-2" src="https://assets.website-files.com/61f2a069c6678de4aa2afb91/61f7e8b534c23c79f99c8c36_articles_stat-lines.svg" alt="">
+        <span>Explore expert videos</span>
+        <h3>Growth of the most popular stocks in 2023</h3>
+        <div class="go-trade-container__grid__2__actions">
+          <p>Play video for more</p>
+          <button type="button" name="button"><font-awesome-icon icon="arrow-right" /></i></button>
+        </div>
       </div>
+
       <div class="go-trade-container__grid__3">
-
+        <span>Explore expert videos</span>
+        <h3>Measure your portfolio value correctly</h3>
+        <div class="go-trade-container__grid__3__actions">
+            <p>Play video for more</p>
+            <button type="button" name="button"><font-awesome-icon icon="arrow-right" /></i></button>
+        </div>
       </div>
-      <div class="go-trade-container__grid__4">
 
+      <div class="go-trade-container__grid__4">
+        <img class="go-trade-container__grid__4__img" src="https://assets.website-files.com/61f2a069c6678de4aa2afb91/61f2c2be7b5f0e126e4d7adf_read_chart.svg" alt="">
+        <span>Explore expert videos</span>
+        <h3>View useful statistics on famous stocks</h3>
+        <div class="go-trade-container__grid__4__actions">
+            <p>Play video for more</p>
+            <button type="button" name="button"><font-awesome-icon icon="arrow-right" /></i></button>
+        </div>
       </div>
     </div>
   </section>
