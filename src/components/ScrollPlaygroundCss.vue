53b13f1dcc1b0f277972f7fcb0582581b4dd1325
Camilo Metaute <milometaute@gmail.com>
2024-01-08 11:02:05 -0500

    playing with layout and scroll effect

--- src/components/ScrollPlaygroundCss.vue
+++ src/components/ScrollPlaygroundCss.vue
@@ -2,7 +2,64 @@
   <section class="scroll-playground-css">
     <div class="progress"></div>
     <div class="container">
-      <h1>Scroll exercise with only css</h1>
+      <h1 class="head-section">Scroll exercise with only css</h1>
+      <p class="text-content">
+        Integer enim neque volutpat ac tincidunt vitae semper. Condimentum
+        lacinia quis vel eros donec ac odio tempor orci. Imperdiet dui accumsan
+        sit amet nulla facilisi morbi tempus. Suspendisse potenti nullam ac
+        tortor vitae. Non sodales neque sodales ut. Elementum eu facilisis sed
+        odio. Aliquet nec ullamcorper sit amet risus nullam eget felis eget.
+        Diam phasellus vestibulum lorem sed risus ultricies tristique. Facilisis
+        sed odio morbi quis. Diam quis enim lobortis scelerisque fermentum dui
+        faucibus. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus
+        at augue eget. Platea dictumst vestibulum rhoncus est pellentesque elit
+        ullamcorper dignissim.
+      </p>
+      <img class="image-content--full" src="https://images.unsplash.com/photo-1465228598882-f82e86db5842?q=80&w=3732&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+      <img class="image-content--half-1" src="https://images.unsplash.com/photo-1703769605297-cc74106244d9?q=80&w=3584&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+      <img class="image-content--half-2" src="https://images.unsplash.com/photo-1445711005973-54fe2a103826?q=80&w=3570&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+      <p class="text-content">
+        Integer enim neque volutpat ac tincidunt vitae semper. Condimentum
+        lacinia quis vel eros donec ac odio tempor orci. Imperdiet dui accumsan
+        sit amet nulla facilisi morbi tempus. Suspendisse potenti nullam ac
+        tortor vitae. Non sodales neque sodales ut. Elementum eu facilisis sed
+        odio. Aliquet nec ullamcorper sit amet risus nullam eget felis eget.
+        Diam phasellus vestibulum lorem sed risus ultricies tristique. Facilisis
+        sed odio morbi quis. Diam quis enim lobortis scelerisque fermentum dui
+        faucibus. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus
+        at augue eget. Platea dictumst vestibulum rhoncus est pellentesque elit
+        ullamcorper dignissim.
+      </p>
+      <img class="image-content--full" src="https://images.unsplash.com/photo-1465228598882-f82e86db5842?q=80&w=3732&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+      <img class="image-content--half-1" src="https://images.unsplash.com/photo-1703769605297-cc74106244d9?q=80&w=3584&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+      <img class="image-content--half-2" src="https://images.unsplash.com/photo-1445711005973-54fe2a103826?q=80&w=3570&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+      <p class="text-content">
+        Integer enim neque volutpat ac tincidunt vitae semper. Condimentum
+        lacinia quis vel eros donec ac odio tempor orci. Imperdiet dui accumsan
+        sit amet nulla facilisi morbi tempus. Suspendisse potenti nullam ac
+        tortor vitae. Non sodales neque sodales ut. Elementum eu facilisis sed
+        odio. Aliquet nec ullamcorper sit amet risus nullam eget felis eget.
+        Diam phasellus vestibulum lorem sed risus ultricies tristique. Facilisis
+        sed odio morbi quis. Diam quis enim lobortis scelerisque fermentum dui
+        faucibus. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus
+        at augue eget. Platea dictumst vestibulum rhoncus est pellentesque elit
+        ullamcorper dignissim.
+      </p>
+      <p class="text-content">
+        Integer enim neque volutpat ac tincidunt vitae semper. Condimentum
+        lacinia quis vel eros donec ac odio tempor orci. Imperdiet dui accumsan
+        sit amet nulla facilisi morbi tempus. Suspendisse potenti nullam ac
+        tortor vitae. Non sodales neque sodales ut. Elementum eu facilisis sed
+        odio. Aliquet nec ullamcorper sit amet risus nullam eget felis eget.
+        Diam phasellus vestibulum lorem sed risus ultricies tristique. Facilisis
+        sed odio morbi quis. Diam quis enim lobortis scelerisque fermentum dui
+        faucibus. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus
+        at augue eget. Platea dictumst vestibulum rhoncus est pellentesque elit
+        ullamcorper dignissim.
+      </p>
+      <img class="image-content--full" src="https://images.unsplash.com/photo-1465228598882-f82e86db5842?q=80&w=3732&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+      <img class="image-content--half-1" src="https://images.unsplash.com/photo-1703769605297-cc74106244d9?q=80&w=3584&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+      <img class="image-content--half-2" src="https://images.unsplash.com/photo-1445711005973-54fe2a103826?q=80&w=3570&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
       <p>
         Integer enim neque volutpat ac tincidunt vitae semper. Condimentum
         lacinia quis vel eros donec ac odio tempor orci. Imperdiet dui accumsan
@@ -15,7 +72,7 @@
         at augue eget. Platea dictumst vestibulum rhoncus est pellentesque elit
         ullamcorper dignissim.
       </p>
-      <p>
+      <p class="text-content">
         Integer enim neque volutpat ac tincidunt vitae semper. Condimentum
         lacinia quis vel eros donec ac odio tempor orci. Imperdiet dui accumsan
         sit amet nulla facilisi morbi tempus. Suspendisse potenti nullam ac
@@ -27,55 +84,7 @@
         at augue eget. Platea dictumst vestibulum rhoncus est pellentesque elit
         ullamcorper dignissim.
       </p>
-      <p>
-        Integer enim neque volutpat ac tincidunt vitae semper. Condimentum
-        lacinia quis vel eros donec ac odio tempor orci. Imperdiet dui accumsan
-        sit amet nulla facilisi morbi tempus. Suspendisse potenti nullam ac
-        tortor vitae. Non sodales neque sodales ut. Elementum eu facilisis sed
-        odio. Aliquet nec ullamcorper sit amet risus nullam eget felis eget.
-        Diam phasellus vestibulum lorem sed risus ultricies tristique. Facilisis
-        sed odio morbi quis. Diam quis enim lobortis scelerisque fermentum dui
-        faucibus. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus
-        at augue eget. Platea dictumst vestibulum rhoncus est pellentesque elit
-        ullamcorper dignissim.
-      </p>
-      <p>
-        Integer enim neque volutpat ac tincidunt vitae semper. Condimentum
-        lacinia quis vel eros donec ac odio tempor orci. Imperdiet dui accumsan
-        sit amet nulla facilisi morbi tempus. Suspendisse potenti nullam ac
-        tortor vitae. Non sodales neque sodales ut. Elementum eu facilisis sed
-        odio. Aliquet nec ullamcorper sit amet risus nullam eget felis eget.
-        Diam phasellus vestibulum lorem sed risus ultricies tristique. Facilisis
-        sed odio morbi quis. Diam quis enim lobortis scelerisque fermentum dui
-        faucibus. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus
-        at augue eget. Platea dictumst vestibulum rhoncus est pellentesque elit
-        ullamcorper dignissim.
-      </p>
-      <p>
-        Integer enim neque volutpat ac tincidunt vitae semper. Condimentum
-        lacinia quis vel eros donec ac odio tempor orci. Imperdiet dui accumsan
-        sit amet nulla facilisi morbi tempus. Suspendisse potenti nullam ac
-        tortor vitae. Non sodales neque sodales ut. Elementum eu facilisis sed
-        odio. Aliquet nec ullamcorper sit amet risus nullam eget felis eget.
-        Diam phasellus vestibulum lorem sed risus ultricies tristique. Facilisis
-        sed odio morbi quis. Diam quis enim lobortis scelerisque fermentum dui
-        faucibus. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus
-        at augue eget. Platea dictumst vestibulum rhoncus est pellentesque elit
-        ullamcorper dignissim.
-      </p>
-      <p>
-        Integer enim neque volutpat ac tincidunt vitae semper. Condimentum
-        lacinia quis vel eros donec ac odio tempor orci. Imperdiet dui accumsan
-        sit amet nulla facilisi morbi tempus. Suspendisse potenti nullam ac
-        tortor vitae. Non sodales neque sodales ut. Elementum eu facilisis sed
-        odio. Aliquet nec ullamcorper sit amet risus nullam eget felis eget.
-        Diam phasellus vestibulum lorem sed risus ultricies tristique. Facilisis
-        sed odio morbi quis. Diam quis enim lobortis scelerisque fermentum dui
-        faucibus. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus
-        at augue eget. Platea dictumst vestibulum rhoncus est pellentesque elit
-        ullamcorper dignissim.
-      </p>
-      <p>
+      <p class="text-content">
         Integer enim neque volutpat ac tincidunt vitae semper. Condimentum
         lacinia quis vel eros donec ac odio tempor orci. Imperdiet dui accumsan
         sit amet nulla facilisi morbi tempus. Suspendisse potenti nullam ac
